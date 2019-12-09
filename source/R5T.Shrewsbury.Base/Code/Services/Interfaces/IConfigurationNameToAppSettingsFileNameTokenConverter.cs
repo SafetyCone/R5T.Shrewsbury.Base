73e1f43462e801e3661888bc81d1bc330ae73b0f
MinexAutomation <minex.automation@gmail.com>
2019-12-08 17:35:56 -0800

    Added configuration-name to appsettings file-name token converter service.

---
+++ source/R5T.Shrewsbury.Base/Code/Services/Interfaces/IConfigurationNameToAppSettingsFileNameTokenConverter.cs
@@ -0,0 +1,10 @@
+﻿using System;
+
+
+namespace R5T.Shrewsbury
+{
+    public interface IConfigurationNameToAppSettingsFileNameTokenConverter
+    {
+        string ConvertConfigurationNameToAppSettingsFileNameToken(string configurationName);
+    }
+}
