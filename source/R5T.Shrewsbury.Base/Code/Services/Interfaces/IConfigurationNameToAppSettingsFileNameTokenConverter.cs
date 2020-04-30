8ae13f278b861926e4a279eafe50c5a3facb066e
MinexAutomation <minex.automation@gmail.com>
2020-04-29 17:09:52 -0700

    Added documentation.

--- source/R5T.Shrewsbury.Base/Code/Services/Interfaces/IConfigurationNameToAppSettingsFileNameTokenConverter.cs
+++ source/R5T.Shrewsbury.Base/Code/Services/Interfaces/IConfigurationNameToAppSettingsFileNameTokenConverter.cs
@@ -3,6 +3,10 @@
 
 namespace R5T.Shrewsbury
 {
+    /// <summary>
+    /// AppSettings files can have a filename token indicating that a particular AppSettings file is for a particular configuration.
+    /// This service gets the filename token corresponding to a configuration name.
+    /// </summary>
     public interface IConfigurationNameToAppSettingsFileNameTokenConverter
     {
         string ConvertConfigurationNameToAppSettingsFileNameToken(string configurationName);
