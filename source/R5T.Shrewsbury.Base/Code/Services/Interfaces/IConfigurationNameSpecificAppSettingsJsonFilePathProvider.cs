5a72b367a1171038e98a6bf7c85fc3326ba94433
MinexAutomation <minex.automation@gmail.com>
2020-06-08 14:58:51 -0700

    Added service definition interfaces.

---
+++ source/R5T.Shrewsbury.Base/Code/Services/Interfaces/IConfigurationNameSpecificAppSettingsJsonFilePathProvider.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+
+namespace R5T.Shrewsbury
+{
+    /// <summary>
+    /// Get the configuration name-specific appsettings.{Configuration}.json file path.
+    /// </summary>
+    /// <remarks>
+    /// Not async since startup is (currently) a synchronous process.
+    /// </remarks>
+    public interface IConfigurationNameSpecificAppSettingsJsonFilePathProvider
+    {
+        string GetConfigurationNameSpecificAppSettingsJsonFilePath();
+    }
+}
---
+++ source/R5T.Shrewsbury.Base/Code/Services/Interfaces/IDefaultAppSettingsJsonFilePathProvider.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+
+namespace R5T.Shrewsbury
+{
+    /// <summary>
+    /// Service to get the default appsettings.json file path.
+    /// </summary>
+    /// <remarks>
+    /// Not async since startup is (currently) a synchronous process.
+    /// </remarks>
+    public interface IDefaultAppSettingsJsonFilePathProvider
+    {
+        string GetDefaultAppSettingsJsonFilePath();
+    }
+}
